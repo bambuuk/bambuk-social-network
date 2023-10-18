40454b2790eb943b0c5563bfdd79717d091d4aa6
Ivan <vanyok.korobka@ukr.net>
2023-10-18 17:11:24 +0300

    upd social network name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,7 +59,7 @@ const Navbar = () => {
     <AppBar position="sticky">
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-          Lama Dev
+          Bambuuk
         </Typography>
         <PetsIcon sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
